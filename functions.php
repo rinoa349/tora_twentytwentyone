d01a1de0f0d46feab08955ed12d71d65257b117b
rinoa349 <chan2s@icloud.com>
2024-02-13 16:44:15 +0900

    Initial commit

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+  wp_enqueue_style( 'parent-style', get_parent_theme_file_uri() . '/style.css' );
+}
+?>
